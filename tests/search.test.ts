62759a9b689d98ced1b76b9adfd473d6d6ccadd0
lerok <kozachenko.lera@gmail.com>
2025-02-07 18:31:16 +0200

    test: add describe block

--- tests/search.test.ts
+++ tests/search.test.ts
@@ -1,31 +1,35 @@
 import { test } from "./fixtures/fixture";
 import { expect } from "@playwright/test";
 
-test("IS-001 suggestion click opens the correct results page", async ({
-  homePage,
-  searchResultsPage,
-}) => {
-  await test.step("Click on search bar", async () => {
-    await homePage.navigationBar.clickSearchButton();
-  });
-
-  const randomSuggestion =
-    await test.step("Select a random search suggestion", async () => {
-      return await homePage.navigationBar.searchContainer.selectRandomSearchSuggestion();
+test.describe("Search", () => {
+  test("IS-001 suggestion click opens the correct results page", async ({
+    homePage,
+    searchResultsPage,
+  }) => {
+    await test.step("Click on search bar", async () => {
+      await homePage.navigationBar.clickSearchButton();
     });
 
-  const keywords =
-    await test.step("Extract keywords from selected suggestion", async () => {
-      return homePage.navigationBar.searchContainer.extractWords(
-        randomSuggestion
+    const randomSuggestion =
+      await test.step("Select a random search suggestion", async () => {
+        return await homePage.navigationBar.searchContainer.selectRandomSearchSuggestion();
+      });
+
+    const keywords =
+      await test.step("Extract keywords from selected suggestion", async () => {
+        return homePage.navigationBar.searchContainer.extractWords(
+          randomSuggestion
+        );
+      });
+
+    await test.step("Verify that search results header contains suggestion keywords", async () => {
+      await Promise.all(
+        keywords.map(async (word) => {
+          await expect(searchResultsPage.getHeaderLocator()).toContainText(
+            word
+          );
+        })
       );
     });
-
-  await test.step("Verify that search results header contains suggestion keywords", async () => {
-    await Promise.all(
-      keywords.map(async (word) => {
-        await expect(searchResultsPage.getHeaderLocator()).toContainText(word);
-      })
-    );
   });
 });
