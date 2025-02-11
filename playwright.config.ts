5c719c5c183da53295bbf3f5d1b0136703048710
lerok <kozachenko.lera@gmail.com>
2025-02-11 18:20:06 +0200

    fix: Decrease expect timeout to 5000ms and adjust CI worker count to improve test performance

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,16 +16,16 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   expect: {
-    timeout: 10000,
+    timeout: 5000,
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 5,
+  workers: process.env.CI ? 2 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["github"], ["html"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
