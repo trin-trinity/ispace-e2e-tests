34263f83c915f3195c5ce6f6c10029dde53c51a4
lerok <kozachenko.lera@gmail.com>
2025-02-17 13:39:28 +0200

    fix: Reorder visibility checks in navigateTo method for improved reliability

--- app/pages/base/BasePage.ts
+++ app/pages/base/BasePage.ts
@@ -8,13 +8,13 @@ export abstract class BasePage extends BaseView {
   protected async navigateTo(url: string = "/", assertions?: IPageAssertions) {
     await this.page.goto(url);
 
-    if (assertions?.waitForIdentifiableElement) {
-      this.waitFor(this.identifiableElement);
-    }
-
     if (assertions?.waitForResponseUrl) {
       const url = assertions?.waitForResponseUrl;
       await this.page.waitForResponse(url);
+    }
+
+    if (assertions?.waitForIdentifiableElement) {
+      this.waitFor(this.identifiableElement);
     }
   }
 }
