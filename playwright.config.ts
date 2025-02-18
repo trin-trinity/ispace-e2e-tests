15de12d345b44f49d285b4479f822d4427321cad
lerok <kozachenko.lera@gmail.com>
2025-02-18 22:24:27 +0200

    chore: Update Playwright configuration to include dotenv support and adjust timeout settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,24 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from "dotenv";
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
-  expect: {
-    timeout: 5000,
-  },
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 0 : 0,
-  workers: process.env.CI ? 3 : 5,
+  workers: process.env.CI ? 3 : 3,
   reporter: "html",
   use: {
     baseURL: "https://ispace.ua/ua",
     trace: "on-first-retry",
     screenshot: "only-on-failure",
+    video: "retain-on-failure",
+  },
+  expect: {
+    timeout: 30_000,
   },
 
   projects: [
