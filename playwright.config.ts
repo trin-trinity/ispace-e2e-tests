b8b5d4dff97ec725068ed037195d193928c12abf
lerok <kozachenko.lera@gmail.com>
2025-02-22 18:52:27 +0200

    fix: Update dotenv dependency and correct path resolution in configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 import dotenv from "dotenv";
 import path from 'path';
 
-dotenv.config({ path: path.resolve(__filename, ".env") });
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 export default defineConfig({
   testDir: "./tests",
